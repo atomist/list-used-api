fee1b337034b89886308a830e06fb87d8cabf534
Taylor Wicksell <twicksell@netflix.com>
2019-11-01 15:38:14 -0700

    use gradleWrapper when available
    fix regex in GradleClassPathResolver

--- src/main/kotlin/com/atomist/javatooling/listusedapi/sourcepath/GradleSourcePathDetector.kt
+++ src/main/kotlin/com/atomist/javatooling/listusedapi/sourcepath/GradleSourcePathDetector.kt
@@ -3,18 +3,19 @@ package com.atomist.javatooling.listusedapi.sourcepath
 import com.atomist.javatooling.listusedapi.classpath.runCommand
 import java.io.File
 import java.io.FileWriter
+import java.lang.RuntimeException
 
 class GradleSourcePathDetector: SourcePathDetector {
     private val INIT_SCRIPT = """
-allprojects {
-    task printSourceSets {
-        doLast {
-            println "sourcepaths:" + project.sourceSets.collectMany {
-               it.getAllJava().getSrcDirTrees().collect { it.dir } 
-            }.join(",")
+    allprojects {
+        task printSourceSets {
+            doLast {
+                println "sourcepaths:" + project.sourceSets.collectMany {
+                   it.getAllJava().getSrcDirTrees().collect { it.dir } 
+                }.join(",")
+            }
         }
     }
-}
 """
 
     override fun getSourcePaths(path: String): Set<String> {
@@ -23,10 +24,25 @@ allprojects {
             writer.append(INIT_SCRIPT)
             writer.flush()
         }
-        val output = ("gradle --init-script " + initGradle.absolutePath + " printSourceSets").runCommand(File(path))
+        val wrapperPath = getWrapperPath(path);
+        val output = ("$wrapperPath --init-script " + initGradle.absolutePath + " printSourceSets").runCommand(File(path))
         val regex = Regex("sourcepaths[=:](.*)")
         return regex.findAll(output!!)
                 .flatMap { r -> r.groups[1]!!.value.splitToSequence(",") }
                 .toSet()
+    }
+
+    fun getWrapperPath(path: String) : String {
+        val wrapperPath = File("$path/gradlew")
+        val parentPath = File(path).parent
+        if(wrapperPath.exists()) {
+            return wrapperPath.absolutePath
+        } else if(File("$path/settings.gradle").exists()) {
+            return "gradle"
+        } else if(parentPath != null) {
+            return getWrapperPath(File(path).parent)
+        } else {
+            return "gradle"
+        }
     }
 }
