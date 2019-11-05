5578b2a599427221b077dc8bd87f04d94dbd6f84
lievendoclo <lieven@atomist.com>
2019-11-05 17:17:26 +0100

    Fix Maven code

--- src/main/kotlin/com/atomist/javatooling/listusedapi/sourcepath/MavenSourcePathDetector.kt
+++ src/main/kotlin/com/atomist/javatooling/listusedapi/sourcepath/MavenSourcePathDetector.kt
@@ -3,8 +3,8 @@ package com.atomist.javatooling.listusedapi.sourcepath
 import java.io.File
 
 class MavenSourcePathDetector: SourcePathDetector {
-    override fun getSourcePaths(path: String): Set<String> {
-        return setOf(path + File.separator + "src/main/java",
-                path + File.separator + "src/test/java");
+    override fun getSourcePaths(path: String): Set<ModuleSourcePath> {
+        return setOf(ModuleSourcePath(path, setOf(path + File.separator + "src/main/java",
+                path + File.separator + "src/test/java")))
     }
 }
